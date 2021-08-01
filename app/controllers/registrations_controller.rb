4280c451941ef7c68d1b0a1467b3e3072f0f6a2e
Joshua Ivie <joshua@lifeonline.com.ng>
2021-08-01 12:13:25 +0100

    Update Form Styling
    Signed-off-by: Joshua Ivie <joshua@lifeonline.com.ng>

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,5 +8,4 @@ class RegistrationsController < Devise::RegistrationsController
   def account_update_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
   end
-  
 end
