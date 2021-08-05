927c8edfd6a9f15724b4edbb899697b1e363aa5b
Joshua Ivie <joshua@lifeonline.com.ng>
2021-08-05 10:22:02 +0100

    Update Author Name for Authenticated Users
    Signed-off-by: Joshua Ivie <joshua@lifeonline.com.ng>

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  def previous_post
+    Post.where("id < ?", id).order(id: :desc).limit(1).first
+  end
+
+  def next_post
+    Post.where("id > ?", id).order(id: :asc).limit(1).first
+  end
 end
