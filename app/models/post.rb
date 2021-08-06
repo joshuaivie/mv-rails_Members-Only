a6a9250d0cb0b5c7b7095ca86cf5ca906cbc786f
Joshua Ivie <joshua@lifeonline.com.ng>
2021-08-06 09:37:10 +0100

    Fix Linter errors
    Signed-off-by: Joshua Ivie <joshua@lifeonline.com.ng>

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,10 @@ class Post < ApplicationRecord
   belongs_to :user
 
   def previous_post
-    Post.where("id < ?", id).order(id: :desc).limit(1).first
+    Post.where('id < ?', id).order(id: :desc).limit(1).first
   end
 
   def next_post
-    Post.where("id > ?", id).order(id: :asc).limit(1).first
+    Post.where('id > ?', id).order(id: :asc).limit(1).first
   end
 end
